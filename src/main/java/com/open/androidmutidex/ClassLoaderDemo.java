4d070ea74be95d6c5a9bdbcd3da79ba80392663a
mustangkong <mustang2247@gmail.com>
2017-12-03 12:05:00 +0800

    add java resource

--- src/main/java/com/open/androidmutidex/ClassLoaderDemo.java
+++ src/main/java/com/open/androidmutidex/ClassLoaderDemo.java
@@ -1,9 +1,16 @@
 package com.open.androidmutidex;
 
+import java.util.ArrayList;
+
 public class ClassLoaderDemo {
     public static void main(String[] args){
         Class mainClass = ClassLoaderDemo.class;
         ClassLoader mainLoadder = mainClass.getClassLoader();
         System.out.println("mainloader is Name: " + mainLoadder.toString());
+
+        Class listClass = ArrayList.class;
+        ClassLoader listLoader = listClass.getClassLoader();
+
+        System.out.println("listLoader is Name: " + listLoader.toString());
     }
 }
